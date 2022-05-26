07087453f104bdd0332c40bb0d9086b1e9c96290
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-26 14:02:11 +0200

    $0013: [CarList] [Confirm]: add confirm button and style

--- components/Confirm.js
+++ components/Confirm.js
@@ -1,9 +1,32 @@
 import React from 'react'
 
+const style = {
+    wrapper: `flex-1 h-full flex flex-col justify-between`,
+    rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
+    confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
+    confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
+}
+
 const Confirm = () => {
-  return (
-    <div>Confirm</div>
-  )
+
+    const storeTripDetails = async () => {}
+
+    return (
+        <div className={style.wrapper}>
+            <div className={style.rideSelectorContainer}>
+            </div>
+            <div className={style.confirmButtonContainer}>
+                <div className={style.confirmButtonContainer}>
+                    <div
+                        className={style.confirmButton}
+                        onClick={() => storeTripDetails()}
+                    >
+                        Confirm UberX
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Confirm
