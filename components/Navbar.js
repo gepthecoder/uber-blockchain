f22ce39701ab04adb33c0f24efd8327da0960072
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-26 12:03:22 +0200

    $0006: [Home] [Navbar]: create right menu bar with username, avatar and conditional rendering for current acc - auth

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+import avatar from '../assets/icons/avatar.png'
+import { BsPerson } from 'react-icons/bs'
+
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
   leftMenu: `flex gap-3`,
@@ -10,15 +14,41 @@ const style = {
   loginText: `ml-2`,
 }
 
+const currentAccount = '0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E'
+
 export const Navbar = () => {
 
   return (
     <div className={style.wrapper}>
+
       <div className={style.leftMenu}>
         <div className={style.logo}>Uber</div>
         <div className={style.menuItem}>Ride</div>
         <div className={style.menuItem}>Drive</div>
         <div className={style.menuItem}>More</div>
+      </div>
+
+      <div className={style.rightMenu}>
+        <div className={style.menuItem}>Help</div>
+        <div className={style.menuItem}>G3P</div>
+        <div className={style.userImageContainer}>
+            <Image
+                className={style.userImage}
+                src={avatar}
+                width={40}
+                height={40}
+            />
+        </div>
+        {currentAccount ? (
+          <div>
+            {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
+          </div>
+        ) : (
+          <div className={style.loginButton} onClick={() => connectWallet()}>
+            <BsPerson />
+            <span className={style.loginText}>Log in</span>
+          </div>
+        )}
       </div>
     </div>
   )
