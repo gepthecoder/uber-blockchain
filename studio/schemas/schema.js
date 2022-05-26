2b8371858adfa3462724b87d3abe1a7a20c8d80f
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-26 16:03:05 +0200

    $0020: [Sanity] [Schemas]: create rides schema doc

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -2,8 +2,9 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import { userSchema } from './userSchema'
+import { ridesSchema } from './ridesSchema'
 
 export default createSchema({
     name: 'default',
-    types: schemaTypes.concat([userSchema]),
+    types: schemaTypes.concat([userSchema, ridesSchema]),
 })
