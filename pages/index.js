f539ec8d52e5f8a156c58ee04785315836f2870c
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-26 12:16:18 +0200

    $0007: [LocationSelector] [MapBox]: create map component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Navbar } from "../components/Navbar"
+import Map from "../components/Map"
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
@@ -13,7 +14,7 @@ export default function Home() {
     <div className={style.wrapper}>
       <Navbar />
     <div className={style.main}>
-      {/* <Map /> */}
+      <Map />
     </div>
     <div className={style.rideRequestContainer}>
       <div className={style.rideRequest}>
