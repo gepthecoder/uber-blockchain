7d5f40927e58fc92f563b2639fe8108dc9e29cb1
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-27 10:35:22 +0200

    $0027: [Uber] [Context]: setup get location coordinated (pick up, drop off)

--- context/uberContext.js
+++ context/uberContext.js
@@ -2,8 +2,56 @@ import { createContext, useState, useEffect } from 'react'
 
 export const UberContext = createContext()
 
-export const UberProvider = ({children}) => {
 
+
+export const UberProvider = ({children}) => {
+    const [pickup, setPickup] = useState('')
+    const [dropoff, setDropoff] = useState('')
+    const [pickupCoordinates, setPickupCoordinates] = useState()
+    const [dropoffCoordinates, setDropoffCoordinates] = useState()
+
+    const createLocationCoordinatePromise = (locationName, locationType) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const response = await fetch('api/map/getLocationCoordinates', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                  body: JSON.stringify({
+                    location: locationName,
+                  }),
+                })
+        
+                const data = await response.json()
+        
+                if (data.message === 'success') {
+                  switch (locationType) {
+                    case 'pickup':
+                      setPickupCoordinates(data.data)
+                      break
+                    case 'dropoff':
+                      setDropoffCoordinates(data.data)
+                      break
+                  }
+                  resolve()
+                } else {
+                  reject()
+                }
+            } catch (error) {
+                console.error(error)
+                reject()
+              }
+        })
+    }
+
+    useEffect(() => {
+        if(pickup && dropoff){
+            ;(async () => {
+                await Promise.all([])
+            })()
+        } else return
+    }, [pickup, dropoff])
 
     return (
         <UberContext.Provider
